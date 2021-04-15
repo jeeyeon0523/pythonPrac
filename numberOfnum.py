b806410161b4b93cebf04c15915fd2cb2f4b432d
jeeyeon.park <jeeyeon0523@gmail.com>
2021-04-15 16:23:00 +0900

    20210415

---
+++ numberOfnum.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == "__main__":
+    A = int(input())
+    B = int(input())
+    C = int(input())
+
+    mul = A*B*C
+
+    arr = [0 for _ in range(10)]
+    s_mul = str(mul)
+
+    for i in range(len(s_mul)):
+        s = int(s_mul[i])
+        arr[s] += 1
+
+    for i in range(10):
+        print(arr[i])
