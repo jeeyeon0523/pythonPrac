fc5ae2566d97bb2128318059bfec0194acb5a982
jeeyeon.park <jeeyeon0523@gmail.com>
2021-04-19 18:41:18 +0900

    20210419

---
+++ stack.py
@@ -0,0 +1,27 @@
+import sys;
+
+if __name__ == '__main__':
+    N = int(input())
+    stack = []
+    for t in range(N):
+        temp = input()
+        cmd = temp.split()[0]
+
+        if cmd == 'push':
+            stack.append(temp.split()[1])
+
+        elif cmd == 'top':
+            print(stack[-1] if len(stack) > 0 else -1)
+
+        elif cmd == 'size':
+            print(len(stack))
+
+        elif cmd == 'empty':
+            print(0 if len(stack) > 0 else 1)
+
+        elif cmd == 'pop':
+            if stack:
+                print(stack.pop())
+            else:
+                print(-1)
+
